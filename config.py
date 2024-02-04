7352a9601c81cc75cb0eda80f06d55d6b3b3c24c
datallah@umich.edu <datallah@umich.edu>
2024-02-03 19:06:32 -0700

    adjusting config code errors

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ def filepath():
   Identifies whether the user is running notebooks from GLC or Colab and initializes filepath.
   """
   from os import environ
-  env_var = os.environ.keys()
+  env_var = environ.keys()
   if 'CLUSTER_NAME' in env_var:
     return '/home/datallah-jaymefis-gibsonce/'
   elif 'COLAB_JUPYTER_IP' in env_var:
@@ -13,11 +13,12 @@ def filepath():
     return '/content/drive/MyDrive/RtGender/'
 
 # specify a file or list of files to load and process
-def extract_dfs(filenames):
+def extract_dfs(filepath, filenames):
   """
   Accepts a string or list of filenames to read as DataFrames.
 
   Parameters:
+  - filepath: path to shared drive where data files reside.
   - filenames: a string or list of strings corresponding to RtGender files.
 
   Returns:
@@ -28,18 +29,18 @@ def extract_dfs(filenames):
   # iterate and load dict with dfs
   for file in filenames:
     file_temp = file[:-4] if file[-4:0] == '.csv' else file
-    df_temp = pd.read_csv(extract_path + file_temp + '.csv').reset_index()
+    df_temp = pd.read_csv(filepath + file_temp + '.csv').reset_index()
     df_temp['sourceID'] = df_temp['source'] + df_temp['index'].astype(str)
     df_dict[file] = df_temp
     return df_dict
 
 # split dfs and save to drive
-def load_dfs(load_path, df_dict):
+def load_dfs(filepath, df_dict):
   """
   Take preprocessed DataFrames and write them to a shared location.
 
   Parameters:
-  - load_path: a string that represents where the files will be written to.
+  - filepath: path to shared drive where data files reside.
   - df_dict: a dictionary of DataFrames to be written to the shared location.
   """
   for key in df_dict:
@@ -48,7 +49,7 @@ def load_dfs(load_path, df_dict):
     y = df_temp['op_gender']
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size = 0.2, random_state = 42)
-    X_train.to_csv(load_path + key + '_X_train.csv')
-    X_test.to_csv(load_path + key + '_X_test.csv')
-    y_train.to_csv(load_path + key + '_y_train.csv')
-    y_test.to_csv(load_path + key + '_y_test.csv')
+    X_train.to_csv(filepath + 'trns/' + key + '_X_train.csv')
+    X_test.to_csv(filepath + 'trns/' + key + '_X_test.csv')
+    y_train.to_csv(filepath + 'trns/' + key + '_y_train.csv')
+    y_test.to_csv(filepath + 'trns/' + key + '_y_test.csv')
